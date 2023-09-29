530c08a4c6ea33f69a691a46c238d1a99772b978
Oamp1796 <oamp1796@DESKTOP-N4BEG1L>
2023-09-28 22:31:55 -0500

    Agregando Inicio Flask

---
+++ WEBDATA-HRI/HRI.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    answers = []
+    for index in range(1, 4):  # Por ahora 3 preguntas
+        answer = request.form.get(f'answer{index}')
+        answers.append(answer)
+    
+    # Send answers to your ML model using requests library
+    prediction = send_to_ml_model(answers)
+    
+    return render_template('result.html', prediction=prediction)
+
+# Implement the send_to_ml_model function to interact with your ML model
+
+if __name__ == '__main__':
+    app.run(debug=True)
