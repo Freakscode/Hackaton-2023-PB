380ad3f1d2cd07e3e2e3abd452f7135768ab8e37
Oamp1796 <oamp1796@DESKTOP-N4BEG1L>
2023-09-28 23:29:21 -0500

    Actualización Orden Carpetas

--- WEBDATA-HRI/templates/app.py
+++ WEBDATA-HRI/templates/app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request
 
-HRI = Flask(__name__)
+app = Flask(__name__)
 
-@HRI.route('/')
+@app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
-"""@HRI.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
     answers = []
-    for index in range(1, 4):  # Por ahora 3 preguntas
+    for index in range(1, 4):  # Assuming 3 questions
         answer = request.form.get(f'answer{index}')
         answers.append(answer)
     
     # Send answers to your ML model using requests library
     prediction = send_to_ml_model(answers)
     
-    return render_template('result.html', prediction=prediction)"""
+    return render_template('result.html', prediction=prediction)
 
 # Implement the send_to_ml_model function to interact with your ML model
 
-# Corrida de la aplicación  
 if __name__ == '__main__':
-    HRI.run(debug=True)
+    app.run(debug=True)
+
