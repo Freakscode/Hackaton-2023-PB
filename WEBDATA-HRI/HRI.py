5367337b64db14d0cfa9035562d758577d437cb9
Oamp1796 <oamp1796@DESKTOP-N4BEG1L>
2023-09-28 23:04:58 -0500

    Actualización HRI.py

--- WEBDATA-HRI/HRI.py
+++ WEBDATA-HRI/HRI.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+HRI = Flask(__name__)
 
-@app.route('/')
+@HRI.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(index.html)
 
-@app.route('/submit', methods=['POST'])
+"""@HRI.route('/submit', methods=['POST'])
 def submit():
     answers = []
     for index in range(1, 4):  # Por ahora 3 preguntas
@@ -16,10 +16,10 @@ def submit():
     # Send answers to your ML model using requests library
     prediction = send_to_ml_model(answers)
     
-    return render_template('result.html', prediction=prediction)
+    return render_template('result.html', prediction=prediction)"""
 
 # Implement the send_to_ml_model function to interact with your ML model
 
 # Corrida de la aplicación  
 if __name__ == '__main__':
-    app.run(debug=True)
+    HRI.run(debug=True)
