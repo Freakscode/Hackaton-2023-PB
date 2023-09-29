ef0f823ca08e272f4bec4da155f65d096e1764f0
Oamp1796 <oamp1796@DESKTOP-N4BEG1L>
2023-09-29 01:07:34 -0500

    Actualización Vista 2 respuesta

--- WEBDATA-HRI/app.py
+++ WEBDATA-HRI/app.py
@@ -2,23 +2,42 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Función de Prueba
+def mock_ml_model(answers):
+    # Simulate the ML model behavior
+    prediction = "Mock Prediction"
+    analysis = "This is a mock analysis."
+    suggestions = "These are mock suggestions."
+    return prediction, analysis, suggestions
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    answers = []
-    for index in range(1, 4):  # Asumiendo 3 preguntas
-        answer = request.form.get(f'answer{index}')
-        answers.append(answer)
-    
-    # Send answers to your ML model using requests library
-    prediction = send_to_ml_model(answers)
-    
-    return render_template('result.html', prediction=prediction)
+ # Define the Flask route handling function for the index page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        answers = []
+        for index in range(1, 4):  # Assuming 3 questions
+            answer = request.form.get(f'answer{index}')
+            answers.append(answer)
+       
+        # Use the mock_ml_model function for testing
+        prediction, analysis, suggestions = mock_ml_model(answers)
+       
+        return render_template('result.html', analysis=analysis, suggestions=suggestions)
+
+    return render_template('index.html') 
 
 # Implement the send_to_ml_model function to interact with your ML model
+# Función de Envió de Respuestas 
+"""def send_to_ml_model(answers):
+    # Implement code to send data to your ML model and receive a prediction, analysis, and suggestions
+    # You may need to use the requests library or another suitable method to communicate with your model
+    
+    # Example:
+    prediction = "Some prediction"
+    analysis = "Some analysis"
+    suggestions = "Some suggestions"
+    
+    return prediction, analysis, suggestions"""
 
 if __name__ == '__main__':
     app.run(debug=True)
