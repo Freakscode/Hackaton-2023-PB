7f8c0e12f30d5e43e628621fb885ee0cf72fb6c6
jbernalg <jeinffersonbernal@gmail.com>
2023-10-18 08:38:57 -0500

    archivos para correr el modelo desde el frontend

---
+++ predict_model.py
@@ -0,0 +1,25 @@
+import joblib
+import pandas as pd
+import sklearn
+
+def funct_pred(datos):
+    modelo = joblib.load('GradientBoosting.pkl')
+    scaler = joblib.load('scaler.pkl')
+
+    # escalar datos
+    nuevos_datos = scaler.transform(pd.DataFrame(datos))
+    y_pred = modelo.predict(nuevos_datos)
+    return y_pred
+
+'''
+datos =  {
+    'Ejercicio': [1], #Sí
+    'Sexo': [1], #Hombre
+    'Altura_(cm)': [151.0],
+    'Peso_(kg)': [50.0],
+    'Historial_de_Tabaco': [0],
+    'Consumo_de_Alcohol': [0]
+}
+
+print(funct_pred(datos))
+'''
