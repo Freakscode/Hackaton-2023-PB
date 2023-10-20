72d8c1fa05e5338b3cd9608aa9b5c774d67f74c0
Oamp1796 <oamp1796@gmail.com>
2023-10-20 14:11:57 -0500

    Act

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length 
 
+
 # clase para agregar las validaciones a las diferentes entradas
 class User(FlaskForm):
     nombre=StringField('Nombre', validators=[
@@ -33,6 +34,7 @@ def template():
     
     return render_template('index.html', form=form) 
 
+
 # Función de Prueba para el Modelo
 def funct_pred(datos):
     modelo = joblib.load('GradientBoosting.pkl')
@@ -43,11 +45,13 @@ def funct_pred(datos):
     y_pred = modelo.predict(nuevos_datos)
     return y_pred
 
+
 # Tener en cuenta que se pueden estos detalles de abajo a la función que convoca el modelo. 
 """
 analysis= ""
 suggesstions=""
 return prediction, analysis, suggestions"""
+
 
  # Ruta y Función que devuelve el template Result, considerando los métodos GET y POST
 @app.route('/form', methods=['GET', 'POST'])
@@ -98,58 +102,3 @@ def form():
 if __name__ == '__main__':
     app.run(debug=True)
     #print(Alcohol)
-
-
-
-
-
-
-
-
-
-"""app = Flask(__name__)
-
-# Función de Prueba
-def mock_ml_model(answers):
-    # Simulate the ML model behavior
-    prediction = "Mock Prediction"
-    analysis = "This is a mock analysis."
-    suggestions = "These are mock suggestions."
-    return prediction, analysis, suggestions
-
- # Define the Flask route handling function for the index page
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    global answers
-    if request.method == 'POST':
-        answers = []
-        for index in range(1, 8):  # Assuming 3 questions
-            answer = request.form.get(f'answer{index}')
-            answers.append(answer)
-       
-        # Use the mock_ml_model function for testing
-        prediction, analysis, suggestions = mock_ml_model(answers)
-        print(answers)
-       
-        return render_template('result.html', analysis=analysis, suggestions=suggestions)
-
-    return render_template('index.html') 
-
-# Está función será la que defina el envio de los datos al modelo
-# Función de Envió de Respuestas 
-def send_to_ml_model(answers):
-    # Implement code to send data to your ML model and receive a prediction, analysis, and suggestions
-    # You may need to use the requests library or another suitable method to communicate with your model
-    
-    # Example:
-    prediction = "Some prediction"
-    analysis = "Some analysis"
-    suggestions = "Some suggestions"
-    
-    return prediction, analysis, suggestions
-"""
-
-"""if __name__ == '__main__':
-    app.run(debug=True)
-    print(answers)"""
-
